a122bd0169051f41240b4dcb85961175fdcd0596
Soeren Becker <soeren.becker@tu-berlin.de>
2022-12-25 18:36:13 +0100

    Cleanup install

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-#
-#
-#
-#
 DOTFOLDERS="tmux fish nvim"
 
 for folder in $DOTFOLDERS; dof 
     echo $folder
     stow  --target=$HOME --restow $folder
 done
-#stow --verbose --target=$HOME --restow $DOTFOLDERS
