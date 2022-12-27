2373aaab570ad15f6637a035ee087bd323260af3
Soeren Becker <soeren.becker@tu-berlin.de>
2022-12-27 14:40:45 +0100

    Add ansible based installation of dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# check if virtualenv already exists
+if [[ ! -d .venv ]]; then
+    echo "Create virtualenv and install ansible"
+    virtualenv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+    ansible-galaxy install -r requirements.yml
+    echo "Run ansible-playbook bootstrap.yml to install dependencies"
+fi
+
 DOTFOLDERS="tmux fish nvim"
 
 for folder in $DOTFOLDERS; do 
