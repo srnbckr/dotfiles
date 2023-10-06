12efce42a3e4dea24bed679672d6216523ad7047
Soeren Becker <soeren.becker@tu-berlin.de>
2023-10-06 12:25:52 +0200

    init zoxide

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -55,3 +55,6 @@ set -g fish_pager_color_progress $comment
 set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
+
+# Init zoxide
+zoxide init fish | source
