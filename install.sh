dc8409425f73f584c347b0367c48a805f2af2bfd
srnbckr <soeren.becker@logsight.ai>
2026-04-26 18:40:00 +0200

    build: add Fisher plugin manager for fish shell

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 DOTFOLDERS="tmux fish nvim bin"
 
+# Install Fisher if not present
+if ! command -v fisher &> /dev/null; then
+    echo "Installing Fisher..."
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish > /tmp/fisher.fish
+    fish -c "source /tmp/fisher.fish"
+    rm /tmp/fisher.fish
+    
+    # Install plugins from fishfile
+    if [ -f ~/.config/fish/fishfile ]; then
+        echo "Installing Fisher plugins..."
+        fish -c "fisher install < ~/.config/fish/fishfile"
+    fi
+fi
+
 for folder in $DOTFOLDERS; do 
     echo $folder
     stow  --target=$HOME --restow $folder
